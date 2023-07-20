f8d28ba1588d5850832395b419d5de73ec4cb2b9
2rang2 <139103064+2rang2@users.noreply.github.com>
2023-07-20 15:38:32 +0900

    Update calculator.swift

--- calculator.swift
+++ calculator.swift
@@ -1,1 +1,32 @@
+class Calculator { 
+        
+    func calculate(op:String, firstNumber: Double, secondNumber: Double) -> Double {
+        switch op {
+            case "+":
+                return firstNumber + secondNumber
+            case "-":
+                return firstNumber - secondNumber
+            case "*":
+                return firstNumber * secondNumber
+            case "/":
+                return firstNumber / secondNumber
+            case "%":
+                let intFirstNumber = Int(firstNumber)
+                let intSecondNumber = Int(secondNumber)
+                return Double(intFirstNumber % intSecondNumber)
+//                return firstNumber % secondNumber
+            default:
+                return 0
+            }
+    }
+}
 
+let calculator = Calculator()
+var a = 6.2
+var b = 4.3
+
+print("addResult : \(calculator.calculate(op:"+", firstNumber:a, secondNumber:b))")
+print("subtractResult : \(calculator.calculate(op:"-", firstNumber:a, secondNumber:b))")
+print("multiplyResult : \(calculator.calculate(op:"*", firstNumber:a, secondNumber:b))")
+print("divideResult : \(calculator.calculate(op:"/", firstNumber:a, secondNumber:b))")
+print("remainderResult : \(calculator.calculate(op:"%", firstNumber:a, secondNumber:b))")
