40b5edf8e3a3688675a48778440529e5546da123
2rang2 <139103064+2rang2@users.noreply.github.com>
2023-07-21 14:38:09 +0900

    Update calculator.swift
    Lv3

--- calculator.swift
+++ calculator.swift
@@ -1,33 +1,32 @@
 import Foundation
 
-class Calculator {    
+class Calculator {
+    var (add, subtract, multiply, divide): (AddOperation, SubtractOperation, MultiplyOperation, DivideOperation)
+    init(add: AddOperation, subtract: SubtractOperation, multiply: MultiplyOperation, divide: DivideOperation) {
+        self.add = add
+        self.subtract = subtract
+        self.multiply = multiply
+        self.divide = divide
+    }    
     func calculate(op:String, firstNumber: Double, secondNumber: Double) -> Double {
             switch op {
                 case "+":
-                    return firstNumber + secondNumber
+                    return add.operate(firstNumber: a, secondNumber:b)
                 case "-":
-                    return firstNumber - secondNumber
+                    return subtract.operate(firstNumber: a, secondNumber:b)
                 case "*":
-                    return firstNumber * secondNumber
+                    return multiply.operate(firstNumber: a, secondNumber:b)
                 case "/":
-                    return firstNumber / secondNumber
-                case "%":
-                    let intFirstNumber = Int(firstNumber)
-                    let intSecondNumber = Int(secondNumber)
-                    return Double(intFirstNumber % intSecondNumber)
+                    return divide.operate(firstNumber: a, secondNumber:b)
                 default:
                     return 0
                 }
     }
  }
 
-let add = AddOperation()
-let subtract = SubtractOperation()
-let multiply = MultiplyOperation()
-let divide = DivideOperation()
+let calculator = Calculator(add: AddOperation(), subtract: SubtractOperation(), multiply: MultiplyOperation(), divide: DivideOperation())
 var a = 6.2
 var b = 4.3
-
 
 class AddOperation {
     func operate(firstNumber: Double, secondNumber: Double) -> Double {
@@ -53,7 +52,7 @@ class DivideOperation {
     }
 }
 
-print("addResult : \(add.operate(firstNumber:a, secondNumber:b))")
-print("subtractResult : \(round(subtract.operate(firstNumber:a, secondNumber:b)*100)/100)")
-print("multiplyResult : \(multiply.operate(firstNumber:a, secondNumber:b))")
-print("divideResult : \(divide.operate(firstNumber:a, secondNumber:b))")
+print("addResult : \(calculator.calculate(op: "+", firstNumber:a, secondNumber:b))")
+print("subtractResult : \(round(calculator.calculate(op: "-", firstNumber:a, secondNumber:b)*100)/100)")
+print("multiplyResult : \(calculator.calculate(op: "*", firstNumber:a, secondNumber:b))")
+print("divideResult : \(calculator.calculate(op: "/", firstNumber:a, secondNumber:b))")
