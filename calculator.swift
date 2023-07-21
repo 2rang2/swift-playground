0923f8ff0adac915957e93378f91c41509d4f0ec
2rang2 <139103064+2rang2@users.noreply.github.com>
2023-07-21 14:08:10 +0900

    Update calculator.swift
    Lv3 구현(?)

--- calculator.swift
+++ calculator.swift
@@ -1,32 +1,59 @@
-class Calculator { 
-        
-    func calculate(op:String, firstNumber: Double, secondNumber: Double) -> Double {
-        switch op {
-            case "+":
-                return firstNumber + secondNumber
-            case "-":
-                return firstNumber - secondNumber
-            case "*":
-                return firstNumber * secondNumber
-            case "/":
-                return firstNumber / secondNumber
-            case "%":
-                let intFirstNumber = Int(firstNumber)
-                let intSecondNumber = Int(secondNumber)
-                return Double(intFirstNumber % intSecondNumber)
-//                return firstNumber % secondNumber
-            default:
-                return 0
-            }
-    }
-}
+import Foundation
 
-let calculator = Calculator()
+class Calculator {    
+    func calculate(op:String, firstNumber: Double, secondNumber: Double) -> Double {
+            switch op {
+                case "+":
+                    return firstNumber + secondNumber
+                case "-":
+                    return firstNumber - secondNumber
+                case "*":
+                    return firstNumber * secondNumber
+                case "/":
+                    return firstNumber / secondNumber
+                case "%":
+                    let intFirstNumber = Int(firstNumber)
+                    let intSecondNumber = Int(secondNumber)
+                    return Double(intFirstNumber % intSecondNumber)
+                default:
+                    return 0
+                }
+    }
+ }
+
+let add = AddOperation()
+let subtract = SubtractOperation()
+let multiply = MultiplyOperation()
+let divide = DivideOperation()
 var a = 6.2
 var b = 4.3
 
-print("addResult : \(calculator.calculate(op:"+", firstNumber:a, secondNumber:b))")
-print("subtractResult : \(calculator.calculate(op:"-", firstNumber:a, secondNumber:b))")
-print("multiplyResult : \(calculator.calculate(op:"*", firstNumber:a, secondNumber:b))")
-print("divideResult : \(calculator.calculate(op:"/", firstNumber:a, secondNumber:b))")
-print("remainderResult : \(calculator.calculate(op:"%", firstNumber:a, secondNumber:b))")
+
+class AddOperation {
+    func operate(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber + secondNumber
+    }
+}
+
+class SubtractOperation {
+    func operate(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber - secondNumber
+    }
+}
+
+class MultiplyOperation {
+    func operate(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber * secondNumber
+    }
+}
+
+class DivideOperation {
+    func operate(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber / secondNumber
+    }
+}
+
+print("addResult : \(add.operate(firstNumber:a, secondNumber:b))")
+print("subtractResult : \(round(subtract.operate(firstNumber:a, secondNumber:b)*100)/100)")
+print("multiplyResult : \(multiply.operate(firstNumber:a, secondNumber:b))")
+print("divideResult : \(divide.operate(firstNumber:a, secondNumber:b))")
